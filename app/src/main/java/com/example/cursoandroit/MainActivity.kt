6106b6f6b7bf8f509d0b66f31efca1eae34d34af
Calebjihe <cajimenez@ikeasistencia.com>
2021-06-03 17:35:46 -0500

    Navegar entre fragmentos

--- app/src/main/java/com/example/cursoandroit/MainActivity.kt
+++ app/src/main/java/com/example/cursoandroit/MainActivity.kt
@@ -8,37 +8,17 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.commit
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var texto:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        texto = findViewById<TextView>(R.id.txt_output)
-        val boton1 =findViewById<Button>(R.id.btn_accion)
-
-        boton1.setOnClickListener {
-            navegarSegundaActivity()
-        }
-        Log.d("Lifecycle", "onCreate: ")
-    }
-
-    private fun navegarSegundaActivity(){
-        val intent = Intent(this, SegundaActivity::class.java)
-        intent.putExtra("nombre","Curso Androit")
-        startActivityForResult(intent,1)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1){
-            if(resultCode == Activity.RESULT_OK){
-                val nombre = data?.getStringExtra("nombre2")
-                texto.text = nombre
-            }
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.fragment_container_view, PrimerFragment())
         }
     }
 
---
+++ app/src/main/java/com/example/cursoandroit/SegundoFragment.kt
@@ -0,0 +1,15 @@
+package com.example.cursoandroit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SegundoFragment : Fragment(R.layout.fragment_segundo) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
